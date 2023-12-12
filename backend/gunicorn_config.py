398691cbba1b776920c83ad34348553fe2a6f291
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-12-12 10:17:03 +0100

    sub modules

--- backend/gunicorn_config.py
+++
@@ -1,3 +0,0 @@
-bind = "0.0.0.0:8000"
-workers = 4
-module = "api.v1.app:app"
--- backend/setup_dev.sql
+++
@@ -1,8 +0,0 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS KNS_dev_db;
-CREATE USER IF NOT EXISTS 'KNS_dev'@'localhost' IDENTIFIED BY 'KNS_dev_pwd';
-GRANT ALL PRIVILEGES ON `KNS_dev_db`.* TO 'KNS_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'KNS_dev'@'localhost';
-GRANT PROCESS ON *.* TO 'KNS_dev'@'localhost';
-FLUSH PRIVILEGES;
